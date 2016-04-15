a8729e0ba8e9fa9e38c3be6b0603448dec5612c1
Alex Nachlas <anachlas@gmail.com>
2016-04-15 08:03:59 -0400

    Put map in Earth section. Moved code into index.html and draw_map.js.

---
+++ js/draw_map.js
@@ -0,0 +1,65 @@
+d3.json("data/20160408.locations.json", function(error, json) {
+    if (error) return console.warn(error);
+
+    // Datamaps expect data in format:
+    // { "USA": { "fillColor": "#42a844", numberOfWhatever: 75},
+    //   "FRA": { "fillColor": "#8dc386", numberOfWhatever: 43 } }
+    var series = json;
+    // We need to colorize every country based on "numberOfWhatever"
+    // colors should be uniq for every value.
+    // For this purpose we create palette(using min/max series-value)
+    var onlyValues = series.map(function(obj) {
+        return obj[1];
+    });
+    var minValue = Math.min.apply(null, onlyValues),
+        maxValue = Math.max.apply(null, onlyValues);
+    // create color palette function
+    // color can be whatever you wish
+    var paletteScale = d3.scale.linear()
+        .domain([minValue, maxValue])
+        .range(["#EFEFFF", "#02386F"]); // blue color
+    // fill dataset in appropriate format
+    var dataset = {};
+    series.forEach(function(item) { //
+        // item example value ["USA", 70]
+        var iso = item[0],
+            value = item[1];
+        dataset[iso] = {
+            numberOfThings: value,
+            fillColor: paletteScale(value)
+        };
+    });
+    // render map
+    new Datamap({
+        element: document.getElementById('mapcontainer'),
+        // projection: 'mercator', // big world map
+        // countries don't listed in dataset will be painted with this color
+        fills: {
+            defaultFill: '#F5F5F5'
+        },
+        data: dataset,
+        geographyConfig: {
+            borderColor: '#DEDEDE',
+            highlightBorderWidth: 2,
+            // don't change color on mouse hover
+            highlightFillColor: function(geo) {
+                return geo['fillColor'] || '#F5F5F5';
+            },
+            // only change border
+            highlightBorderColor: '#B7B7B7',
+            // show desired information in tooltip
+            popupTemplate: function(geo, data) {
+                // don't show tooltip if country don't present in dataset
+                if (!data) {
+                    return;
+                }
+                // tooltip content
+                return ['<div class="hoverinfo">',
+                    '<strong>', geo.properties.name, '</strong>',
+                    '<br>Count: <strong>', data.numberOfThings, '</strong>',
+                    '</div>'
+                ].join('');
+            }
+        }
+    });
+});
