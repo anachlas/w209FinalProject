5414aae86880e61ff714f708a93254b9ed33af3d
Thomas Atkins <thomasatkins@Thomass-MacBook.local>
2016-04-16 10:15:23 +1000

    code to extract urls into json file

--- extract_locations.py
+++ extract_locations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import json
 import datetime
+import numpy as np
 
 yesterday = datetime.date.fromordinal(datetime.date.today().toordinal()-1)
 filename = yesterday.strftime('data/%Y%m%d.export.CSV')
@@ -9,21 +10,27 @@ f = open(filename)
 
 locations = {}
 
+print 'processing CSV'
 for l in f:
 	data = l.split('\t')
 	loc = data[15][:3]
+	url = data[57].strip()
 	if len(loc) !=3:
 		continue
 	if loc in locations.keys():
-		locations[loc] += 1
+		count = locations[loc][0]+1
+		v = locations[loc][1]
+		locations[loc] = [count,v]
 	else:
-		locations[loc] = 1
-	print loc
+		locations[loc] = [1,url]
 
 loc_list = []
 for l in locations.keys():
-	loc_list.append([l,locations[l]])
+	loc_list.append([l,np.log(locations[l][0]),locations[l][1]])
 
 
-with open('./data/20160408.locations.json', 'w') as fp:
+print 'writing JSON'
+with open(yesterday.strftime('data/%Y%m%d.locations.json'), 'w') as fp:
     json.dump(loc_list, fp)
+
+print 'finished'
